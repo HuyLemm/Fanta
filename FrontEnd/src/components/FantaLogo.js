b16d3b98cfadd17b7ba209f0df090710c408e8f6
Ethan_nguyen <phuthanh0301@gmail.com>
2024-06-12 20:41:06 +0700

    user icon update

--- FrontEnd/src/components/FantaLogo.js
+++ FrontEnd/src/components/FantaLogo.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import fantaImage from '../assets/images/fanta.png';
+import userIcon from '../assets/images/user.png'; 
 import styles from '../assets/styles/fanta.module.css';
 
 const Logofanta = () => {
-  const navigate = useNavigate(); // Thêm dòng này
+  const [showDropdown, setShowDropdown] = useState(false);
+  const navigate = useNavigate();
+  const dropdownRef = useRef(null);
 
   const handleLoginClick = () => {
-    navigate('/login'); // Thêm dòng này
+    navigate('/login');
+  };
+
+  const handleClickOutside = (event) => {
+    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+      setShowDropdown(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const toggleDropdown = (event) => {
+    setShowDropdown((prevShowDropdown) => !prevShowDropdown);
   };
 
   return (
@@ -15,8 +35,13 @@ const Logofanta = () => {
       <a href="/">
         <img src={fantaImage} className={styles.fanta} alt="Fanta" />
       </a>
-      <div className={styles.login}>
-        <button onClick={handleLoginClick} className={styles.loginButton}>Login</button>
+      <div className={styles.userContainer} ref={dropdownRef}>
+        <div className={styles.userIcon} onClick={toggleDropdown}>
+          <img src={userIcon} alt="User Icon" />
+        </div>
+        <div className={`${styles.dropdown} ${showDropdown ? styles.dropdownVisible : ''}`}>
+          <button onClick={handleLoginClick} className={styles.loginButton}>Login</button>
+        </div>
       </div>
     </header>
   );
