1bc301f829e163c3e528acf3847671139e3dcc0a
Ethan_nguyen <phuthanh0301@gmail.com>
2024-06-13 12:49:54 +0700

    Footer added

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -3,13 +3,17 @@ import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './assets/styles/app.css';
 import AppRouter from './routes/AppRoutes';
 import Logofanta from './components/FantaLogo';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Router>
     <div className="App">   
       <Logofanta />  
-      <AppRouter />
+      <div className="content">
+        <AppRouter />
+      </div>
+      <Footer />
     </div>
     </Router>
   );
