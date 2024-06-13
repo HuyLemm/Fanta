21fb49e36925da210ec1a2b2d3cbc4b98ba187d7
Ethan_nguyen <phuthanh0301@gmail.com>
2024-06-13 16:54:35 +0700

    admin,user,login button

--- FrontEnd/src/pages/HomePage.js
+++ FrontEnd/src/pages/HomePage.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from '../assets/styles/HomePage.module.css';
 import Carousel from '../components/auth/Carousel';
 import GenreSection from '../components/auth/GenreSection';
 
 function HomePage() {
+  useEffect(() => {
+    if (!sessionStorage.getItem('isRefreshed')) {
+      sessionStorage.setItem('isRefreshed', 'true');
+      window.location.reload();
+    } else {
+      sessionStorage.removeItem('isRefreshed');
+    }
+  }, []);
+
   return (
     <div className={styles.homePage}>
       <Carousel />
